fdce0d0ba828c8ec22991ea62cf0a7f5d04aecc1
raunaqjabbal <raunaq.jabbal@bytelearn.ai>
2023-12-05 16:33:27 +0530

    Cleaned useless code

--- LIHQ/procedures/face_align/face_crop.py
+++ LIHQ/procedures/face_align/face_crop.py
@@ -1,15 +1,6 @@
-from pathlib import Path
-from io import BytesIO
-from matplotlib import pyplot as plt
-from PIL import Image
 import torchvision
-from IPython import display
-import numpy as np
 from .shape_predictor import align_face
-from drive import open_url
 import dlib
-from mpl_toolkits.axes_grid1 import ImageGrid
-from bicubic import BicubicDownSample
 import gdown
 import os
 ######################################
