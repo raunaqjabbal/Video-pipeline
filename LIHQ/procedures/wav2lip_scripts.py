da2c233c9b1e62617f20b7a671c18635dbf09967
raunaqjabbal <raunaq.jabbal@bytelearn.ai>
2023-12-06 17:20:08 +0530

    Updating Wav2lip

--- LIHQ/procedures/wav2lip_scripts.py
+++ LIHQ/procedures/wav2lip_scripts.py
@@ -13,13 +13,13 @@ def wav2lip_run(adir):
   command = f'python inference.py --checkpoint_path checkpoints/wav2lip.pth --face {vid_path} --audio {aud_path} --outfile {out_path}  --pads 0 20 0 0'
   try:
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-    while True:
-        output = process.stdout.readline()
-        if output == '' and process.poll() is not None:
-            break
-        if output:
-            print(output.strip())
-    print(f"Script execution completed with exit code:", process.poll())
+    # while True:
+    #     output = process.stdout.readline()
+    #     if output == '' and process.poll() is not None:
+    #         break
+    #     if output:
+    #         print(output.strip())
+    # print(f"Script execution completed with exit code:", process.poll())
   except subprocess.CalledProcessError:
     print('!!!!!!! Error with Wav2Lip Paths !!!!!!')
     sys.exit()
