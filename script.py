2e8414a657e4d4c31c6601445a57b4626ce98f08
raunaqjabbal <raunaq.jabbal@bytelearn.ai>
2023-12-06 15:52:29 +0530

    Updating Script

--- script.py
+++ script.py
@@ -1,5 +1,16 @@
+
+import sys
+import os
+sys.path.append(os.path.join("LIHQ", "first_order_model"))
+sys.path.append(os.path.join("LIHQ", "procedures"))
+os.environ['SUNO_OFFLOAD_CPU'] = 'True'
+os.environ['SUNO_USE_SMALL_MODELS'] = 'True'
+
+
 import nltk
 nltk.download("punkt")
 
+
 from bark.generation import preload_models
 preload_models(text_use_small=True, coarse_use_small=True, fine_use_small=True)
+
